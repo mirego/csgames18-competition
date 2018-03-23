b32a3993d250d9291b713a03f8b2c65a1e73bcd7
Hugo Lefrancois <hlefrancois@mirego.com>
2018-03-22 22:54:30 -0400

    PartCellView

--- ios/MapPing/Classes/UI/List/ListView.swift
+++ ios/MapPing/Classes/UI/List/ListView.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ListView: UIView {
 
+    private let partCellView = PartCellView()
+
     init() {
         super.init(frame: .zero)
         backgroundColor = .white
+
+        partCellView.configure(partImageName: "part-sensor", title: "Bougie 4W", subTitle: "Moteur principal", coordinates: "46.7552° N, 71.2265° W", distance: "(0.62 km)")
+        addSubview(partCellView)
     }
 
     required init(coder aDecoder: NSCoder) {
@@ -20,5 +25,6 @@ class ListView: UIView {
 
     override func layoutSubviews() {
         super.layoutSubviews()
+        partCellView.pin.top().horizontally()
     }
 }
--- ios/MapPing/Classes/UI/Stylesheet.swift
+++ ios/MapPing/Classes/UI/Stylesheet.swift
@@ -9,14 +9,22 @@ import UIKit
 
 class Stylesheet {
     static func appearance() {
-        UINavigationBar.appearance().barTintColor = .cooper
+        UINavigationBar.appearance().barTintColor = .copper
         UINavigationBar.appearance().isTranslucent = false
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.font: UIFont(name: "LeagueSpartan-Bold", size: 17)!, NSAttributedStringKey.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.font: UIFont.leagueSpartanBold(17), NSAttributedStringKey.foregroundColor: UIColor.white]
     }
 }
 
 extension UIColor {
     class var primary: UIColor { return UIColor(hex: 0xF7D239)! }
     class var secondary: UIColor { return UIColor(hex: 0xC5C4C3)! }
-    class var cooper: UIColor { return UIColor(hex: 0xd1582a)! }
+    class var copper: UIColor { return UIColor(hex: 0xd1582a)! }
+    class var purpleBrown: UIColor { return UIColor(hex: 0x231f20)! }
+    class var brownishGrey: UIColor { return UIColor(hex: 0x6b6a6a)! }
+}
+
+extension UIFont {
+    class func leagueSpartanBold(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "LeagueSpartan-Bold", size: size)!
+    }
 }
