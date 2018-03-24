70f4d672a43a045fb0821bae12e66055d56b21da
Le-non <leanne.dutil@gmail.com>
2018-03-24 19:42:35 -0400

    Added Location stub

---
+++ android/app/src/main/java/com/mirego/csmapapplication/model/Location.java
@@ -0,0 +1,20 @@
+package com.mirego.csmapapplication.model;
+
+/**
+ * Created by leanne on 24/03/18.
+ */
+
+public class Location {
+    public String name;
+    public String type;
+    public String address;
+    public String component;
+    public String notes;
+    public double lat;
+    public double lon;
+
+
+    public Location(){
+        
+    }
+}
