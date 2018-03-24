bc7ee7c0c7cfb8df26c722080bbfdb5c033749b6
Jack McCracken <jack.mccracken@shopify.com>
2018-03-24 19:29:47 -0400

    Fix

--- ios/MapPing/Classes/Domain/Model/Part.swift
+++ ios/MapPing/Classes/Domain/Model/Part.swift
@@ -10,13 +10,22 @@ struct Part: Codable {
         case name
         case latitude = "lat"
         case longitude = "lon"
+        case type
+        case notes
+        case address
+        case component
     }
 
     let name: String
     let latitude: Float
     let longitude: Float
-    let type: Int
+    let type: String
     let notes: String
     let address: String
     let component: String
+    var formattedCoordinates : String {
+        let formattedLat = "\(abs(latitude))° \(latitude < 0 ? "S" : "N")"
+        let formattedLong = "\(abs(longitude))° \(longitude < 0 ? "W" : "E")"
+        return "\(formattedLat), \(formattedLong)"
+    }
 }
