c788229f6dca642b0b57d726fb8df76ca0001f2e
matherrien <matherrien@hotmail.ca>
2018-03-24 18:58:50 -0400

    add mapping model

---
+++ android/app/src/main/java/com/mirego/csmapapplication/model/mapping.java
@@ -0,0 +1,70 @@
+package com.mirego.csmapapplication.model;
+
+public class mapping {
+    private String name;
+    private String component;
+    private String notes;
+    private String type;
+    private String lat;
+    private String lng;
+    private String address;
+
+    public mapping() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
