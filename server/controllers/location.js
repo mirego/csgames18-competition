2d56150573dc7e9eed86aa5ea7b7ddc88eaee1a3
Lance Lafontaine <lancelafontaine@protonmail.com>
2018-03-24 21:24:55 -0400

    Just filters out points which don't have latitude and longitude for now, geocoding script seems to be removing some parts

--- server/controllers/location.js
+++ server/controllers/location.js
@@ -1,15 +1,9 @@
 const mappingGateway = require('../gateways/mapping')
-const util = require('../util/util')
 
 module.exports = {
 
     getDistanceBetweenPoints(req, res) {
-        const possibleOptionsResponse = util.respondToOptionsMethod(req, res)
-        if (possibleOptionsResponse) return possibleOptionsResponse
-
         let currentPoint = req.body
-        console.log(currentPoint)
-        console.log(currentPoint)
         console.log(currentPoint)
         if (!currentPoint || !currentPoint.lat || !currentPoint.lon) {
             return res.status(422).send({"message":"You must supply the 'lat' and 'lon' fields for your point."})
@@ -18,8 +12,10 @@ module.exports = {
         let allParts = mappingGateway.findAllParts()
         let newParts = []
         for (part of allParts) {
-            part.distance = Math.sqrt(part.lat*part.lat + part.lon*part.lon)
-            newParts.push(part)
+            if (part.lat && part.lon) {
+                part.distance = Math.sqrt(part.lat*part.lat + part.lon*part.lon)
+                newParts.push(part)
+            }
         }
 
         return res.json(newParts)
